16bf932ed33adc7e796a0b6907c7ffc196119ce9
Philip A Cook <cookpa@pennmedicine.upenn.edu>
2020-11-05 15:25:28 +0000

    WIP: Need to test ANTsRNet build

---
+++ docker/ANTsRNet/installANTsRNet.R
@@ -0,0 +1,11 @@
+install.packages("devtools", repos = "https://mirrors.nics.utk.edu/cran", dependencies = T)
+
+library(devtools)
+
+# turning off upgrades allows us to build from an ANTsR base image
+# without upgrading ANTsR
+install_github( "ANTsX/ANTsRNet", upgrade = "never" )
+
+# Installs tensorflow and python bindings 
+library(keras) 
+install_keras(method = c("auto", "virtualenv", "conda"), conda = "auto", version = "default", tensorflow = "default", extra_packages = c("tensorflow-hub"))
